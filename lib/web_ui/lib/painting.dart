ca7289be348b6a63e9ef2a51fae3f2a6e855f73a
ead <ead@tvip.ru>
2023-09-14 10:08:45 +0300

    [ROSITA] Squash commits before: 2024.06.11
    [ROSITA] Override transform method to RositaSurfacePath
    
    [ROSITA] Provide PathRefIterator as RositaPathRefIterator and SPathVerb as RositaSPath
    
    [ROSITA] Provide path as RositaSurfacePath
    
    [Rosita] Migration to Flutter 3.19.3
    
    [Rosita] Squash commits before: 2023.12.26
    
    [Rosita] Fix for Chrome 52
    
      - unsupported applyGlobalCssRulesToSheet
      - unsupported DomShadowRoot
    
    [Rosita] Add class RositaGradient
    
    [Rosita] Add class RositaBlurImageFilter

--- lib/web_ui/lib/painting.dart
+++ lib/web_ui/lib/painting.dart
@@ -298,7 +298,7 @@ abstract class Gradient implements Shader {
     Float64List? matrix4,
   ]) {
     final Float32List? matrix = matrix4 == null ? null : engine.toMatrix32(matrix4);
-    return engine.renderer.createLinearGradient(
+    return RositaGradientLinearShader(
       from,
       to,
       colors,
@@ -327,7 +327,7 @@ abstract class Gradient implements Shader {
     } else {
       assert(center != Offset.zero ||
           focal != Offset.zero); // will result in exception(s) in Skia side
-      return engine.renderer.createConicalGradient(
+      return RositaGradientRadialShader(
         focal, focalRadius, center, radius, colors, colorStops, tileMode, matrix32);
     }
   }
@@ -339,7 +339,7 @@ abstract class Gradient implements Shader {
     double startAngle = 0.0,
     double endAngle = math.pi * 2,
     Float64List? matrix4,
-  ]) => engine.renderer.createSweepGradient(
+  ]) => RositaGradientSweepShader(
     center,
     colors,
     colorStops,
@@ -427,7 +427,7 @@ class ImageFilter {
     double sigmaX = 0.0,
     double sigmaY = 0.0,
     TileMode tileMode = TileMode.clamp
-  }) => engine.renderer.createBlurImageFilter(
+  }) => RositaBlurImageFilter(
     sigmaX: sigmaX,
     sigmaY: sigmaY,
     tileMode: tileMode
